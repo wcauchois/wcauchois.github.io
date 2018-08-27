e388901cc4b23809c8a32eb880a853f919c93e3f
Bill Cauchois <wcauchois@gmail.com>
2018-08-26 23:29:50 -0400

    loading and showin repos

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,18 @@ interface AppState {
   name: string;
 }
 
+const USER_NAME = 'wcauchois';
+
 const repoQuery = `
   {
-    user(login: "wcauchois") {
+    user(login: "${USER_NAME}") {
       repositories(first: 100) {
         nodes {
           name
           isFork
           createdAt
           updatedAt
+          url
           description
           primaryLanguage {
             name
@@ -31,6 +34,35 @@ const repoQuery = `
   }
 `;
 
+interface Repository {
+  name: string;
+  isFork: boolean;
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  url: string;
+  primaryLanguage: {
+    name: string,
+  };
+  owner: {
+    login: string;
+  };
+}
+
+interface RepoQueryResponse {
+  user: {
+    repositories: {
+      nodes: Repository[];
+    };
+  };
+}
+
+function filterAndSortRepos(repos: Repository[]) {
+  const newRepos = repos.filter(repo => !repo.isFork && repo.owner.login === USER_NAME);
+  newRepos.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+  return newRepos;
+}
+
 export class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
@@ -39,13 +71,30 @@ export class App extends Component<AppProps, AppState> {
 
   render(props: AppProps, state: AppState) {
     return <Query query={repoQuery}>
-      {(loading: boolean, error: Error, data: any) => {
-        console.log('in renderFn', loading, error, data);
+      {(loading: boolean, error?: Error, data?: RepoQueryResponse) => {
+        if (loading) {
+          return <div>Loading</div>;
+        } else if (error) {
+          return <div>Error</div>;
+        }
+
+        const repos = filterAndSortRepos(data!.user.repositories.nodes);
         return <div>
-          hi hi also<br />
-          {loading.toString()}<br />
-          {error && error.message}<br />
-          {data}
+          <ul>
+            {repos.map(repo => <li class="repo-item">
+              <div class="repo-name">
+                <a href={repo.url} class="repo-link">
+                  {repo.name}
+                </a>
+              </div>
+              <div class="repo-language">
+                {repo.primaryLanguage.name}
+              </div>
+              <div class="repo-description">
+                {repo.description}
+              </div>
+            </li>)}
+          </ul>
         </div>;
       }}
     </Query>;
